88f4089fb72fa37e9347a0c62b6f94ed032ef9a2
Kim Bergstrom <kimmen_b@hotmail.com>
2023-05-10 13:43:04 +0000

    Style: Added popup function for rule button on index.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,19 @@
 console.log("Just checking connections");
+
+// This is the Rule JS //
+
+function openPopup() {
+    // Get the popup element
+    var popup = document.getElementById("popup");
+    
+    // Remove the "hidden" class to show the popup
+    popup.classList.remove("hidden");
+  }
+  
+  function closePopup() {
+    // Get the popup element
+    var popup = document.getElementById("popup");
+    
+    // Add the "hidden" class to hide the popup
+    popup.classList.add("hidden");
+  }
