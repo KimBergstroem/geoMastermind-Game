314d1b1e136f99abb81fdcabf8c1589b2d05879a
Kim Bergstrom <kimmen_b@hotmail.com>
2023-05-12 20:41:20 +0000

    Script: Added an Array with some simple questions in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,9 +17,7 @@ function openPopup() {
 
 
 
-
   // Game Page //
-
   function showQuiz() {
     let mainWrapper = document.getElementById("main-wrapper");
     let quizWrapper = document.getElementById("quiz-wrapper");
@@ -30,8 +28,58 @@ function openPopup() {
   
 
 
+  // Questions / Answers //
+const questions = [
+  {
+    question: "In wich country is this picture from?",
+    answer: [
+      { text: "Paris", correct: false},
+      { text: "London", correct: false},
+      { text: "Sweden", correct: true},
+      { text: "Italy", correct: false},
+    ]
+  },
+  {
+    question: "What type of color have Swedish flag?",
+    answer: [
+      { text: "blue, yellow", correct: true},
+      { text: "green, red", correct: false},
+      { text: "blue, green", correct: false},
+      { text: "white, red and blue", correct: false},
+    ]
+  },
+  {
+    question: "What is the name of Egypts sea?",
+    answer: [
+      { text: "redsea", correct: true},
+      { text: "saltsea", correct: false},
+      { text: "watersea", correct: false},
+      { text: "oceansea", correct: false},
+    ]
+  }
+]
 
+/* Game dashboard content */
+const questionElement = documentgetElementById("question");
+const answerButton = documentgetElementById("answer-button");
+const nextButton = documentgetElementById("next-btn");
 
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion(){
+  let currentQuestion = question[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+}
 
   // Score Dashboard Page //
 
